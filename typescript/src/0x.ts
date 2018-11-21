cfd5308ae84209a2316874f334af48863a0920d0
urugang <urugang@gmail.com>
2018-11-21 15:46:42 +0800

    add typescript 0x example

--- typescript/src/0x.ts
+++ typescript/src/0x.ts
@@ -6,6 +6,38 @@ import {
     Order,
     orderHashUtils,
     signatureUtils,
+    Web3ProviderEngine,
+    RPCSubprovider,
+    ERC20TokenWrapper,
 } from '0x.js';
+import { MnemonicWalletSubprovider } from '@0x/subproviders';
 import { Web3Wrapper } from '@0x/web3-wrapper';
-import { getContractAddressesForNetworkOrThrow } from '@0x/contract-addresses';
+
+
+
+(async function() {
+    const MNEMONIC = 'luggage scissors person arrive daughter stereo order print emotion rule ocean garment';
+    const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
+    const DECIMAL = new BigNumber(18);
+    const ROPSTEN_NETWORK_ID = 3;
+
+
+    const mnemonicWallet = new MnemonicWalletSubprovider({
+        mnemonic: MNEMONIC,
+        baseDerivationPath: BASE_DERIVATION_PATH,
+    });
+    const pe = new Web3ProviderEngine();
+    const rpcUrl = 'https://ropsten.infura.io/';
+    pe.addProvider(mnemonicWallet);
+    pe.addProvider(new RPCSubprovider(rpcUrl));
+    pe.start();
+
+
+    const providerEngine = pe;
+    const web3Wrapper = new Web3Wrapper(providerEngine);
+    const [owner, leftMaker, rightMaker, matcherAccount] = await web3Wrapper.getAvailableAddressesAsync();
+    var erc20Wrapper = new ERC20TokenWrapper(web3Wrapper, ROPSTEN_NETWORK_ID, [owner, leftMaker, rightMaker, matcherAccount], owner);
+    [erc20TokenA] = await erc20Wrapper.deployDummyTokensAsync(1, DECIMAL)
+    console.info("erc20TokenA: ", erc20TokenA);
+    return 0;
+}())
