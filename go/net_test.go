33fb261fdb97271f11bf9a94bb27d8b7efb172ce
urugang <urugang@gmail.com>
2019-01-19 10:15:01 +0800

    update telethon

--- go/net_test.go
+++ go/net_test.go
@@ -11,18 +11,11 @@ import (
 	"testing"
 )
 
-func TestNet(t *testing.T) {
+func TestToutiao(t *testing.T) {
 	t.Skip("skipping testing in short mode")
 
 	urls := []string{
 		"http://www.toutiao.com/i6614608697882575367/?tt_from=weixin&utm_campaign=client_share&wxshare_count=2&from=singlemessage&timestamp=1541278985&app=news_article_lite&utm_source=weixin&iid=47956109968&utm_medium=toutiao_android&group_id=6614608697882575367&pbid=6615894279619495427",
-		"https://www.toutiao.com/i6588744716605456899/?tt_from=weixin&utm_campaign=client_share&wxshare_count=1&timestamp=1541984097&app=news_article_lite&utm_source=weixin&iid=50377168296&utm_medium=toutiao_android&group_id=6588744716605456899",
-		"https://www.toutiao.com/i6599951947556454915/?tt_from=weixin&utm_campaign=client_share&wxshare_count=1&timestamp=1541984241&app=news_article_lite&utm_source=weixin&iid=50377168296&utm_medium=toutiao_android&group_id=6599951947556454915",
-		"https://www.toutiao.com/i6602380745975529987/?tt_from=weixin&utm_campaign=client_share&wxshare_count=1&timestamp=1541984318&app=news_article_lite&utm_source=weixin&iid=50377168296&utm_medium=toutiao_android&group_id=6602380745975529987",
-		"https://www.toutiao.com/i6617954633316827655/?tt_from=weixin&utm_campaign=client_share&wxshare_count=1&timestamp=1541984175&app=news_article_lite&utm_source=weixin&iid=50377168296&utm_medium=toutiao_android&group_id=6617954633316827655",
-		"https://www.toutiao.com/i6617954633316827655/?tt_from=weixin&utm_campaign=client_share&wxshare_count=1&timestamp=1541984175&app=news_article_lite&utm_source=weixin&iid=50377168296&utm_medium=toutiao_android&group_id=6617954633316827655",
-		"https://www.toutiao.com/i6621357196997296643/?tt_from=weixin&utm_campaign=client_share&wxshare_count=1&timestamp=1541732945&app=news_article_lite&utm_source=weixin&iid=50478756673&utm_medium=toutiao_android&group_id=6621357196997296643",
-		"https://www.toutiao.com/i6621372238220231171/?tt_from=weixin&utm_campaign=client_share&wxshare_count=1&timestamp=1541984439&app=news_article_lite&utm_source=weixin&iid=50377168296&utm_medium=toutiao_android&group_id=6621372238220231171",
 	}
 
 	client := &http.Client{}
@@ -51,6 +44,77 @@ func TestNet(t *testing.T) {
 		bufReader := bufio.NewReader(reader)
 
 		var validURL = regexp.MustCompile(`http://p[0-9]*.pstatp.com/large/pgc-image/[a-z0-9]*`)
+		for {
+			if str, err := bufReader.ReadString('\n'); err == nil {
+				result := validURL.FindAllStringSubmatch(str, -1)
+				for index1, v1 := range result {
+					for index2, v2 := range v1 {
+						fmt.Println(index, index1, index2, v2)
+						req, err := http.NewRequest("GET", v2, nil)
+
+						if err != nil {
+							log.Fatalln(err)
+						}
+
+						req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:63.0) Gecko/20100101 Firefox/63.0")
+						req.Header.Set("Accept", "*/*")
+						req.Header.Set("Host", "www.toutiao.com")
+						req.Header.Set("accept-encoding", "gzip, deflate")
+						req.Header.Set("Accept-Language", "zh-CN")
+
+						resp, err := client.Do(req)
+						if err != nil {
+							fmt.Println("failed", err)
+							continue
+						}
+						defer resp.Body.Close()
+						file, err := os.Create(fmt.Sprintf("/tmp/%d-%d-%d.jpeg", index, index1, index2))
+						bufWriter := bufio.NewWriter(file)
+						bufWriter.ReadFrom(resp.Body)
+						defer file.Close()
+					}
+				}
+			} else {
+				break
+			}
+		}
+	}
+}
+
+func TestItiger(t *testing.T) {
+	t.Skip("skipping testing in short mode")
+
+	urls := []string{
+		"http://www.itiger.com/college/basics?id=68",
+	}
+
+	client := &http.Client{}
+
+	for index, urlPath := range urls {
+		req, err := http.NewRequest("GET", urlPath, nil)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:63.0) Gecko/20100101 Firefox/63.0")
+		req.Header.Set("Accept", "*/*")
+		req.Header.Set("Host", "static.itiger.com")
+		req.Header.Set("Accept-encoding", "gzip, deflate")
+		req.Header.Set("Accept-Language", "zh-CN")
+		req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+		reader, err := gzip.NewReader(resp.Body)
+
+		bufReader := bufio.NewReader(reader)
+
+		var validURL = regexp.MustCompile(`http://www.itiger.com/college/basics?id=[0-9]*#.*`)
 		for {
 			if str, err := bufReader.ReadString('\n'); err == nil {
 				result := validURL.FindAllStringSubmatch(str, -1)
--- python/telethon_test.py
+++ python/telethon_test.py
@@ -1,4 +1,4 @@
-from telethon import TelegramClient, sync
+from telethon import TelegramClient
 import socks
 from telethon import types
 import unittest
@@ -6,6 +6,9 @@ import io
 import yaml
 import os
 import logging
+# import _thread
+import asyncio
+import functools
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -25,76 +28,80 @@ class TelethonTestCase(unittest.TestCase):
             self.config = {}
 
     def test_download_urugang(self):
-        self.download("urugang", "+8613588178587")
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.download_dialogs("urugang", "+8613588178587"))
+
 
     def test_download_u(self):
-        self.download("u", "+8613588034774")
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.download_dialogs("u", "+8613588034774"))
+        loop.run_forever()
 
     def test_download_v(self):
-        self.download("v", "+8613732215927")
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.download_dialogs("v", "+8613732215927"))
 
     def test_download_w(self):
-        self.download("w", "+8613868136443")
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.download_dialogs("w", "+8613868136443"))
 
-    def download(self, account, phone):
+    async def download_dialog(self, account, client, dialog):
         try:
-            # 0) setup
-            api_id = 478514
-            api_hash = '6e152cff4b48d83171b509923667ed47'
-            proxy = (socks.SOCKS5, 'localhost', 6153)
-            # proxy = (socks.HTTP, '127.0.0.1', 8087)
-            # proxy = None
-            client = TelegramClient(account, api_id, api_hash, proxy=proxy)
-            # client.session.set_dc(2, '149.154.167.50', 443) # urugang
-            # client.session.set_dc(1, '149.154.175.50', 443)
-
-            # 1) start client
-            client.start(max_attempts=1, phone=phone)
-            # # 2) iterate all dialogs
-            for dialog in client.get_dialogs():
-                key = account + "-" + dialog.name.replace("/", "_")
-                print(self.config)
-                if key not in self.config:
-                    #self.config[key] = dialog.message.id-30000
-                    self.config[key] = 0
-                with io.open('logs/' + key + ".log", 'a', encoding='utf8') as log:
-                    print(dialog.name, ": ",self.config[key])
-                    lastId = None
-                    while True:
-                        if lastId == self.config[key]:
-                            break
-                        lastId = self.config[key]
-                        for message in client.iter_messages(dialog.entity, limit=10, min_id=self.config[key], reverse=True):
-                            print("\n", message.date, ":", message.id, ":", message.message, file=log)
-                            if not isinstance(message, types.Message):
-                                self.config[key]= message.id
-                                continue
-                            media = message.media
-                            if not isinstance(media, (types.MessageMediaDocument)):
-                                self.config[key]= message.id
-                                continue
-                            attrs = media.document.attributes
-                            if not isinstance(attrs[0], (types.DocumentAttributeFilename)):
-                                self.config[key]= message.id
-                                continue
-                            for attr in attrs:
-                                file_size = 0
-                                while [ True ]:
-                                    file_name = "downloads/" + attr.file_name
-                                    if os.path.exists(file_name):
-                                        stat = os.stat(file_name)
-                                        if isinstance(media, types.MessageMediaDocument):
-                                            file_size = stat.st_size
-                                            if file_size == media.document.size:
-                                                break
-                                    print("\n", attr.file_name, "(", media.document.size, "):", file_size, file=log)
-                                    print("\n", attr.file_name, "(", media.document.size, "):", file_size)
-                                    client.download_media(message, file_name)
-                                    break
+            key = account + "-" + dialog.name.replace("/", "_")
+            if key not in self.config:
+                self.config[key] = 0
+            with io.open('logs/' + key + ".log", 'a', encoding='utf8') as log:
+                print(dialog.name, ": ",self.config[key])
+                lastId = None
+                while True:
+                    if lastId == self.config[key]:
+                        break
+                    lastId = self.config[key]
+                    async for message in client.iter_messages(dialog.entity, limit=10, min_id=self.config[key], reverse=True):
+                        print("\n", message.date, ":", message.id, ":", message.message, file=log)
+                        if not isinstance(message, types.Message):
                             self.config[key]= message.id
-        # except RuntimeError as e:
-        #     print("Unexpected error:", e.args)
+                            continue
+                        media = message.media
+                        if not isinstance(media, (types.MessageMediaDocument)):
+                            self.config[key]= message.id
+                            continue
+                        for attr in media.document.attributes:
+                            if not isinstance(attr, (types.DocumentAttributeFilename)):
+                                self.config[key]= message.id
+                                continue
+                            file_size = 0
+                            while [ True ]:
+                                file_name = "downloads/" + attr.file_name
+                                if os.path.exists(file_name):
+                                    stat = os.stat(file_name)
+                                    if isinstance(media, types.MessageMediaDocument):
+                                        file_size = stat.st_size
+                                        if file_size == media.document.size:
+                                            break
+                                print("\n", attr.file_name, "(", media.document.size, "):", file_size, file=log)
+                                print("\n", attr.file_name, "(", media.document.size, "):", file_size)
+                                asyncio.create_task(client.download_media(message, file_name))
+                                break
+                        self.config[key]= message.id
         finally:
             # 3) update config file
             with io.open(self.configFile, 'w', encoding='utf8') as outfile:
                 yaml.dump(self.config, outfile, default_flow_style=False, allow_unicode=True)
+
+    async def download_dialogs(self, account, phone):
+        # 0) setup
+        api_id = 478514
+        api_hash = '6e152cff4b48d83171b509923667ed47'
+        proxy = (socks.SOCKS5, 'localhost', 6153)
+        # proxy = (socks.HTTP, 'phobos.public.opennetwork.cc', 1090, False,
+        #  '353916769', 'dN2TTGIm')
+
+        # proxy = None
+        client = TelegramClient(account, api_id, api_hash, proxy=proxy)
+        # client.session.set_dc(2, '149.154.167.50', 443) # urugang
+        # 1) start client
+        await client.start(max_attempts=1, phone=phone)
+        # # 2) iterate all dialogs
+        for dialog in await client.get_dialogs():
+            await self.download_dialog(account, client, dialog)
