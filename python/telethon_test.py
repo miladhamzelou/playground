fffbe1db3645f12f48b0bdfe1f12e465f156ec96
urugang <urugang@gmail.com>
2019-01-20 10:36:11 +0800

    update telethon

--- python/telethon_test.py
+++ python/telethon_test.py
@@ -35,15 +35,12 @@ class TelethonTestCase(unittest.TestCase):
     def test_download_u(self):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.download_dialogs("u", "+8613588034774"))
-        loop.run_forever()
 
     def test_download_v(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.download_dialogs("v", "+8613732215927"))
+        asyncio.run_until_complete(self.download_dialogs("v", "+8613732215927"))
 
     def test_download_w(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.download_dialogs("w", "+8613868136443"))
+        asyncio.run_until_complete(self.download_dialogs("w", "+8613868136443"))
 
     async def download_dialog(self, account, client, dialog):
         try:
@@ -53,12 +50,13 @@ class TelethonTestCase(unittest.TestCase):
             with io.open('logs/' + key + ".log", 'a', encoding='utf8') as log:
                 print(dialog.name, ": ",self.config[key])
                 lastId = None
+                tasks = []
                 while True:
                     if lastId == self.config[key]:
                         break
                     lastId = self.config[key]
                     async for message in client.iter_messages(dialog.entity, limit=10, min_id=self.config[key], reverse=True):
-                        print("\n", message.date, ":", message.id, ":", message.message, file=log)
+                        print("\n", message.date, ":", message.id, ":", message.message)
                         if not isinstance(message, types.Message):
                             self.config[key]= message.id
                             continue
@@ -81,21 +79,29 @@ class TelethonTestCase(unittest.TestCase):
                                             break
                                 print("\n", attr.file_name, "(", media.document.size, "):", file_size, file=log)
                                 print("\n", attr.file_name, "(", media.document.size, "):", file_size)
-                                asyncio.create_task(client.download_media(message, file_name))
+                                tasks.append(asyncio.create_task(self.download_media(client, message, file_name)))
                                 break
                         self.config[key]= message.id
+
+                    for task in tasks:
+                        await task
         finally:
             # 3) update config file
             with io.open(self.configFile, 'w', encoding='utf8') as outfile:
                 yaml.dump(self.config, outfile, default_flow_style=False, allow_unicode=True)
 
+    async def download_media(self, client, message, file_name):
+        print("========", file_name)
+        await client.download_media(message, file_name)
+        print("--------", file_name)
+
     async def download_dialogs(self, account, phone):
         # 0) setup
         api_id = 478514
         api_hash = '6e152cff4b48d83171b509923667ed47'
-        proxy = (socks.SOCKS5, 'localhost', 6153)
-        # proxy = (socks.HTTP, 'phobos.public.opennetwork.cc', 1090, False,
-        #  '353916769', 'dN2TTGIm')
+        # proxy = (socks.SOCKS5, 'localhost', 1080)
+        proxy = (socks.SOCKS5, 'phobos.public.opennetwork.cc', 1090, False,
+         '353916769', 'dN2TTGIm')
 
         # proxy = None
         client = TelegramClient(account, api_id, api_hash, proxy=proxy)
